515fc65c5f33f2b012f338ab7c46053f26cc4deb
Ratb0t <108471394+Ratb0t@users.noreply.github.com>
2026-01-27 19:31:11 +0000

    ## Завершение подзадачи 2.42, задачи 4.18

--- Part4/4.18/Src/main.c
+++ Part4/4.18/Src/main.c
@@ -4,8 +4,8 @@ int main()
 {
     //test_overflow();
     //print_3times_numbers();
-    print_most_common_numbers();
+    //print_most_common_numbers();
     //struct my_string *s;
-    
+    reverse_sentence();
     return 0;
 }
