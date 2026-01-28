f4c8a69b9f74a06cbd8d4b223f256a282ad396d7
Ratb0t <108471394+Ratb0t@users.noreply.github.com>
2026-01-28 20:50:22 +0000

    Добавление ## Добавление: - Добавлен макрос для поиска минимума - Добавлен макрос для поиска максимума

---
+++ Part4/Utilities/Headers/my_macroses.h
@@ -0,0 +1,8 @@
+
+#ifndef REINCLUDE_PROTECTION_MY_MACROSES
+#define REINCLUDE_PROTECTION_MY_MACROSES
+
+#define Min(a, b) (((a) < (b)) ? (a) : (b))
+#define Max(a, b) (((a) > (b)) ? (a) : (b))
+
+#endif //!REINCLUDE_PROTECTION_MY_MACROSES
