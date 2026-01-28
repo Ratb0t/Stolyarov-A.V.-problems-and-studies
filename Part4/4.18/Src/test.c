5940f9e647df3c9b540d0b14aa9d36c3959a3b9a
Ratb0t <108471394+Ratb0t@users.noreply.github.com>
2026-01-28 20:47:37 +0000

    Рефактор ## Рефактор: - Создана отдельная функция test_str() в которой можно выполнить отладку функций из my_string - Создана отдельная функция test_list() в которой можно выполнить отладку функций из my_list

--- Part4/4.18/Src/test.c
+++ Part4/4.18/Src/test.c
@@ -1,32 +1,56 @@
 #include "my_string.h"
+#include "my_list.h"
 #include <assert.h>
 #include <stdio.h>
-int main()
-{
-    //char control_result;
-    //char *buf;
-    struct my_string *str1 = my_str_create("Hello saivor\n");
-    struct my_string *str2 = my_str_create(" world ");
 
+int test_str()
+{
+
+    printf("%s\n", VERY_LONG_STRING);
+
+    struct my_string *str1 = my_str_create("Hello saivor");
+    struct my_string *str2 = my_str_create(" world ");
+    
     assert(str1 != 0);
 
     printf("%s\n", *(char **)str1);
 
-    //assert(set_string(str1, VERY_LONG_STRING) == 1);
+    // // assert(set_string(str1, VERY_LONG_STRING) == 1);
     printf("%s\n", *(char **)str2);
 
     struct my_string *result1 = my_str_insert(str1, 5, str2);
+    printf("%s\n", my_str_get_data(result1));
+    my_str_destroy(result1);
+
     struct my_string *result2 = my_str_concatenation(str1, str2);
+    printf("%s\n", my_str_get_data(result2));
     my_str_pushback_char(result2, 's');
     my_str_pushback_char(result2, '\n');
-    printf("%s\n", my_str_get_data(result1));
     printf("%s\n", my_str_get_data(result2));
-
+    my_str_destroy(result2);
 
     my_str_destroy(str1);
     my_str_destroy(str2);
-    my_str_destroy(result1);
-    my_str_destroy(result2);
+    
+
+
+    return 1;
+}
+
+int test_list()
+{
+    my_list *lst = my_list_create();
+    my_list_push_front(lst, (void *)22);
+    my_list_delete_item(lst, 0);
+    my_list_destroy(lst);
+
+    return 1;
+}
+
+int main()
+{
+
+    test_str();
 
     return 0;
 }
