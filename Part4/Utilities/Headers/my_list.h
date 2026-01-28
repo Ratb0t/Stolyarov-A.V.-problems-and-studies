c1c3cf398014652e3a428b360405eb80c2befee5
Ratb0t <108471394+Ratb0t@users.noreply.github.com>
2026-01-28 17:57:58 +0000

    Добавление ## Добавление: - Объявление структуры struct node - Введен псевдоним my_list_iterator для struct node * - Введен псевдоним my_list_head и my_list_tail для const struct node ** - Задекларирована struct my_list - Введен синоним my_list для struct my_list - Задекларированы функции my_list_create() my_list_destroy() my_list_revers() my_list_push_front() my_list_push_back() my_list_get_len() my_list_get_item() my_list_get_first() my_list_get_last() my_list_get_head() my_list_get_tail()

---
+++ Part4/Utilities/Headers/my_list.h
@@ -0,0 +1,39 @@
+#ifndef REINCLUDE_PROTECTION_MY_LIST
+#define REINCLUDE_PROTECTION_MY_LIST
+struct node
+{
+    void *data_holder;
+    struct node *next;
+};
+
+typedef struct node *my_list_iterator;
+typedef const struct node **my_list_head, **my_list_tail;
+
+struct my_list;
+typedef struct my_list my_list;
+
+
+my_list *my_list_create();
+/*******************************************************************************/
+int my_list_destroy(my_list *lst);
+/*******************************************************************************/
+int my_list_revers(my_list *lst);
+/*******************************************************************************/
+int my_list_push_front(my_list *list, void *data);
+
+int my_list_push_back(my_list *list, void *data);
+/*******************************************************************************/
+unsigned int my_list_get_len(const my_list *lst);
+
+my_list_iterator my_list_get_item(const my_list *lst, unsigned int pos);
+
+my_list_iterator my_list_get_first(const my_list *lst);
+
+my_list_iterator my_list_get_last(const my_list *lst);
+
+my_list_head my_list_get_head(const my_list *lst);
+
+my_list_tail my_list_get_tail(const my_list *lst);
+/*******************************************************************************/
+
+#endif //! REINCLUDE_PROTECTION_MY_LIST
