f321d0ecd2023e431119f205ab1ea4f529d06763
Ratb0t <108471394+Ratb0t@users.noreply.github.com>
2026-01-28 21:12:13 +0000

    Фикс, Рефактор ## Фикс: - Функция buf_resize() не обновлял **буфер** и **capacity** у строки после копирования - Функция my_str_insert() некоректно вставляла строку, если вставка происходила в конец другой строки ## Рефактор: - Изменена логика buf_resize() присваивание и проверка резутата выделения памяти внесен в if - Функция my_str_create() в случае неудачи возвращает NULL - Функция my_str_insert() в случае неудачи возвращает NULL - Функция my_str_insert() изменена логика расчета capacity для создания результирующей строки - Функция my_str_pushback_char() добавлен пояснительный комментарий для магического +2

--- Part4/Utilities/Src/my_string.c
+++ Part4/Utilities/Src/my_string.c
@@ -47,12 +47,15 @@ int my_memcpy(void *dest, const void *src, size_t n)
 static int buf_resize(struct my_string* str)
 {
     int new_capacity = str->length * CAPACITY_FACTOR;
-    char *new_buf = malloc(new_capacity);
-    if(!new_buf)
-        return 0;;
+    char *new_buf;
+    if (!(new_buf = malloc(new_capacity)))
+        return 0;
 
     my_memcpy(new_buf, str->buf, str->length);
 
+    free(str->buf);
+    str->buf = new_buf;
+    str->capacity = new_capacity;
     return 1;
 }
 
@@ -71,7 +74,7 @@ struct my_string *my_str_create(const char *buf)
 {
     struct my_string *str = malloc(sizeof(struct my_string));
     if (!str)
-        return 0;;
+        return NULL;
 
     str->capacity = DEFAULT_CAPACITY_SIZE;
     if(buf)
@@ -85,9 +88,10 @@ struct my_string *my_str_create(const char *buf)
         if (!str->buf)
         {
             free(str);
-            return 0;;
+            return NULL;
         }
-        my_str_set_string(str, buf);
+        if(!my_str_set_string(str, buf))
+            return NULL;
     }
     else
     {
@@ -96,7 +100,7 @@ struct my_string *my_str_create(const char *buf)
         if (!str->buf)
         {
             free(str);
-            return 0;;
+            return NULL;
         }
     }
 
@@ -107,7 +111,7 @@ struct my_string *my_str_create_with_capacity(int capacity)
 {
     struct my_string *str = malloc(sizeof(struct my_string));
     if (!str)
-        return 0;;
+        return NULL;
     
     str->capacity = capacity;
     str->buf = malloc(capacity);
@@ -115,7 +119,7 @@ struct my_string *my_str_create_with_capacity(int capacity)
     if(!str->buf)
     {
         free(str);
-        return 0;;
+        return NULL;
     }
 
     return str;
@@ -129,7 +133,7 @@ int my_str_set_string(struct my_string *str, const char *buf)
         str->length = 0;
         return 1;
     }
-    int buf_size = my_strlen(buf);
+    int buf_size = my_strlen(buf) + 1; // +1 учитвает '\0'
     
     if (str->capacity < buf_size)
     {
@@ -160,34 +164,41 @@ int my_str_destroy(struct my_string *str)
 struct my_string *my_str_insert(const struct my_string *str1, int pos, const struct my_string *str2)
 {
     if(str1->length < pos)
-        return 0;
-        
-    struct my_string *result = my_str_create_with_capacity((str1->length + str2->length) * CAPACITY_FACTOR);
+        return NULL;
+
+    int capacity = Max((str1->length + str2->length + 1) * CAPACITY_FACTOR, DEFAULT_CAPACITY_SIZE);
+
+    struct my_string *result = my_str_create_with_capacity(capacity); /*сумма длин строк + 1 для '\0'*/
     if(!result)
-        return 0;;
+        return NULL;
     
     result->length = str1->length + str2->length;
 
     //printf("pos %d\n", pos);
     
     int i = 0;
+    //printf("First part\n");
     for (; i < pos; ++i)
     {
         result->buf[i] = str1->buf[i];
+        //printf("%3d %3c\n", result->buf[i], result->buf[i]);
     }
     
     //printf("i %d\n", i);
-    
-    for (int j = 0; str2->buf[j]; ++i, ++j)
+    //printf("Second part\n");
+    for (int j = 0; j <= str2->length; ++i, ++j)
     {
         result->buf[i] = str2->buf[j];
+        //printf("%3d %3c\n", result->buf[i], result->buf[i]);
     }
     
     //printf("i %d\n", i);
-
-    for(int j = pos+1; j <= str1->length; ++j, ++i)
+    //printf("Last part\n");
+    --i;
+    for(int j = pos + 1; j <= str1->length; ++j, ++i)
     {
         result->buf[i] = str1->buf[j];
+        //printf("%3d %3c\n", result->buf[i], result->buf[i]);
         //printf("%d %d\t", i, j);
     }
     //printf("\ncreate_my_string_with_capacity %s\n", result->buf);
@@ -207,10 +218,12 @@ const char *my_str_get_data(const struct my_string *str)
 
 int my_str_pushback_char(struct my_string *str, char ch)
 {
-    if (PERCENT_OF(str->length + 2, str->capacity) >= STRING_RESIZE_THRESHOLD)
-        if(!buf_resize(str))
+    /*Буфер должен вместить в себя символ + '\0'*/
+    if (PERCENT_OF(str->length + 2, str->capacity) >= 
+        STRING_RESIZE_THRESHOLD) 
+        if (!buf_resize(str))
             return 0;
-                
+
     str->buf[str->length] = ch;
     str->length += 1;
     str->buf[str->length + 1] = 0;
