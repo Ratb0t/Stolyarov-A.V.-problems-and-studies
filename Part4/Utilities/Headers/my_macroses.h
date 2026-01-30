f89016db495e3234667d9feed4f90136e3032505
Ratb0t <daniil_leitin@mail.ru>
2026-01-30 20:47:25 +0300

    Добавление ## Добавление: - Создан макрос определяющий, яляется ли переданный символ цифрой

--- Part4/Utilities/Headers/my_macroses.h
+++ Part4/Utilities/Headers/my_macroses.h
@@ -4,5 +4,7 @@
 
 #define Min(a, b) (((a) < (b)) ? (a) : (b))
 #define Max(a, b) (((a) > (b)) ? (a) : (b))
+#define Is_number(ch) (((ch) >= '0') && ((ch) <= '9'))
+
 
 #endif //!REINCLUDE_PROTECTION_MY_MACROSES
