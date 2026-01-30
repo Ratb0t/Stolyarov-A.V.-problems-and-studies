e00883b6cc4d541d9a66fa47774c736d9e7ee204
Ratb0t <daniil_leitin@mail.ru>
2026-01-30 22:21:35 +0300

    Выполнена задача 4.18 (завершил все подзадачи) Выполнена подзадача 2.44

--- Part4/4.18/Src/main.c
+++ Part4/4.18/Src/main.c
@@ -7,6 +7,7 @@ int main()
     //print_most_common_numbers();
     //struct my_string *s;
     //reverse_sentence();
-    print_coloumn_sentence();
+    //print_coloumn_sentence();
+    print_numbers_in_sentence();
     return 0;
 }
