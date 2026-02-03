fb812027c31b64b45425a49f4172735638e30440
Ratb0t <daniil_leitin@mail.ru>
2026-02-03 14:15:30 +0300

    Добавления ## Добавления: - Добавлен макрос определяющий процентное отношение двух чисел

--- Utilities/Headers/my_macroses.h
+++ Utilities/Headers/my_macroses.h
@@ -5,6 +5,6 @@
 #define Min(a, b) (((a) < (b)) ? (a) : (b))
 #define Max(a, b) (((a) > (b)) ? (a) : (b))
 #define Is_number(ch) (((ch) >= '0') && ((ch) <= '9'))
-
+#define PERCENT_OF(n1, n2) (((n1) * 100) / (n2))
 
 #endif //!REINCLUDE_PROTECTION_MY_MACROSES
