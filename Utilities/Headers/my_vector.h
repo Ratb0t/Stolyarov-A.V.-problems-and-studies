7099e1bf44d931c1f89453c96bce1ef73895625f
Ratb0t <daniil_leitin@mail.ru>
2026-02-03 14:28:07 +0300

    Рефактор, Добавления ## Рефактор: - Тип my_vector_iterator с top_type * изменён на void *, для того, чтобы была возможноть разыменовывать указатель и работать с данными напрямую. - Внесение коректировок в соответствии с изменениями - Для геттеров добавлен модификатор const ## Добавления: - Декларация функций my_vector_push_front() my_vector_iter_next() my_vector_get_data() - Добавление констант VECTOR_RESIZE_THRESHOLD и CAPACITY_FACTOR - Включение заголовка с макросами

--- Utilities/Headers/my_vector.h
+++ Utilities/Headers/my_vector.h
@@ -2,25 +2,32 @@
 #define REINCLUDE_PROTECTION_MY_VECTOR
 
 #include "my_types.h"
+#include "my_macroses.h"
 #include "utilities_functions.h"
 #define DEFAULT_CAPACITY 16
+#define VECTOR_RESIZE_THRESHOLD 70
+#define CAPACITY_FACTOR 1.5
 
 typedef struct my_vector my_vector;
 
-typedef top_type *my_vector_iterator;
+typedef void *my_vector_iterator;
 
 
 my_vector *my_vector_create(int data_type_size);
 /*******************************************************************************/
 int my_vector_destroy(my_vector *vec);
 /*******************************************************************************/
-int my_vector_push_back(my_vector *vec, top_type data);
 int my_vector_insert_data(my_vector *vec, unsigned int pos, top_type data);
+int my_vector_push_back(my_vector *vec, top_type data);
+int my_vector_push_front(my_vector *vec, top_type data);
 int my_vector_set_data(my_vector *vec, unsigned int pos, top_type data);
 /*******************************************************************************/
-my_vector_iterator *my_vector_get_first(my_vector *vec);
-my_vector_iterator *my_vector_get_item(my_vector *vec, unsigned int pos);
-my_vector_iterator *my_vector_get_last(my_vector *vec);
+my_vector_iterator my_vector_get_first(const my_vector *vec);
+my_vector_iterator my_vector_get_last(const my_vector *vec);
+my_vector_iterator my_vector_get_item(const my_vector *vec, unsigned int pos);
+int my_vector_iter_next(const my_vector *vec, my_vector_iterator *it);
+int my_vector_get_data(const my_vector *vec, unsigned int pos, top_type *data);
+
 /*******************************************************************************/
 int my_vector_get_size(my_vector *vec);
 
