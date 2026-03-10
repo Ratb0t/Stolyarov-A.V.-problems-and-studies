9bdad2fc5c98385f73e9bd669cd299ee4e1d9dc0
Ratb0t <daniil_leitin@mail.ru>
2026-03-10 18:19:17 +0300

    Добавление ## Добавление: Описана функция create_and_init_context() destroy_context() context_reset_analizator()

--- my_interpreter/Src/Headers/context.h
+++ my_interpreter/Src/Headers/context.h
@@ -1,9 +1,14 @@
 #ifndef REINCLUDE_CONTEXT_SENTRY
 #define REINCLUDE_CONTEXT_SENTRY
 
+#include <stdlib.h>
 #include "my_types.h"
+#include "analyzator.h"
+#include "process_handler.h"
 
 context *create_and_init_context();
 void destroy_context(context *cnt);
+
+void context_reset_analizator(context *cnt);
 
 #endif /* !REINCLUDE_CONTEXT_SENTRY */
--- my_interpreter/Src/context.c
+++ my_interpreter/Src/context.c
@@ -2,9 +2,43 @@
 
 context *create_and_init_context()
 {
-    
+    context *shell = malloc(sizeof(context));
+    if (shell == NULL)
+    {
+        printf("Context create error: Failed to allocate interpreter\n");
+        return NULL;
+    }
+    shell->alzr = create_analyzator();
+    if (shell->alzr == NULL)
+    {
+        free(shell);
+        return NULL;
+    }
+
+    shell->proc_hanler = create_process_handle();
+    if (shell->proc_hanler == NULL)
+    {
+        destroy_analyzator(shell->alzr);
+        free(shell);
+        return NULL;
+    }
+
+    init_analizator(shell->alzr);
+    init_process_handle(shell->proc_hanler);
+    return shell;
 }
 void destroy_context(context *cnt)
 {
+    destroy_analyzator(cnt->alzr);
+    destroy_process_handle(cnt->proc_hanler);
+    free(cnt);
+    return;
+}
 
+void context_reset_analizator(context *cnt)
+{
+    init_analizator(cnt->alzr);
+    analyzator_code_error_handler(cnt->alzr);
+    cnt->code = cnt->alzr->code;
+    return;
 }
